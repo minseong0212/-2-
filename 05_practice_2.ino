dec8342186c896cb90c4232b14d833516607cd71
minseong0212 <diamond5659@naver.com>
2024-09-23 10:41:52 +0900

    Create 05_practice_2.ino

---
+++ 05_practice_2.ino
@@ -0,0 +1,19 @@
+int ledPin = 7; // LED 핀 번호 (예: 13번 핀)
+
+void setup() {
+  pinMode(ledPin, OUTPUT); // LED 핀을 출력 모드로 설정
+}
+
+void loop() {
+  digitalWrite(ledPin, LOW); // LED 끄기
+  delay(1000); // 1초 대기
+
+  for (int i = 0; i < 5; i++) {
+    digitalWrite(ledPin, HIGH); // LED 켜기
+    delay(100); // 0.1초 대기
+    digitalWrite(ledPin, LOW); // LED 끄기
+    delay(100); // 0.1초 대기
+  }
+
+  delay(1000); // 다음 루프 전에 1초 대기
+}
